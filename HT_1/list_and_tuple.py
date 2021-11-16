16e135a46469edd56f8e826a49965132f3b7ce3d
grosslord2017 <grosslord2017@ukr.net>
2021-11-16 09:27:42 +0200

    first task

---
+++ HT_1/list_and_tuple.py
@@ -0,0 +1,6 @@
+'''Script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.'''
+
+subsequence = input().split()
+
+print(list(subsequence))
+print(tuple(subsequence))
