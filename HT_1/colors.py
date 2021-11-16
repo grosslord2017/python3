773b148738db346f2ce47800bf267a7e63f84a2a
grosslord2017 <grosslord2017@ukr.net>
2021-11-16 11:43:25 +0200

    input refactoring

--- HT_1/colors.py
+++ HT_1/colors.py
@@ -2,7 +2,8 @@
 colours from color_list_1 which are not present
 in color_list_2.'''
 
-color_list_1 = set(["White", "Black", "Red"])
-color_list_2 = set(["Red", "Green"])
+color_list_1 = set(list(input('Enter first set: ').split(', ')))
+color_list_2 = set(list(input('Enter second set: ').split(', ')))
 
+print("Result:")
 print(color_list_1.difference(color_list_2))
