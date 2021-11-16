fce2fe8e9bee72ecee001e89b06874d4bd6659df
grosslord2017 <grosslord2017@ukr.net>
2021-11-16 16:30:50 +0200

    correction

--- HT_1/1_list_and_tuple.py
+++ HT_1/1_list_and_tuple.py
@@ -1,6 +1,6 @@
 '''Script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.'''
 
-subsequence = input().split()
+subsequence = input().split(',')
 
 new_list = list(subsequence)
 new_tuple = tuple(subsequence)
