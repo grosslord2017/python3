f551df09af8ac4e81a5b5c600eeee469e2ae1546
grosslord2017 <grosslord2017@ukr.net>
2021-11-19 14:26:25 +0200

    change in 1 task

--- HT_2/1_concatinate.py
+++ HT_2/1_concatinate.py
@@ -1,12 +1,29 @@
 '''Написати скрипт, який конкатенує всі елементи в списку і виведе їх на екран.'''
 
-number_strings = int(input("Enter number of strings: "))
-count = 0
-list_of_strings = []
+hard = [1, 2, 'test', 10, 'foo', 15, 'egg', 'hello']
 
-while number_strings > count:
-    row = input("Enter a string: ")
-    list_of_strings.append(row)
-    count += 1
+#Вариант с "плюсованием" строк
+temp = ''
+for i in hard:
+    temp += str(i)
 
-print(''.join(list_of_strings))
+print(temp)
+
+#Вариант с методом join
+temp = []
+for i in hard:
+    temp.append(str(i))
+
+print(''.join(temp))
+
+#Вариант с пользовательским вводом
+# number_strings = int(input("Enter number of strings: "))
+# count = 0
+# list_of_strings = []
+#
+# while number_strings > count:
+#     row = input("Enter a string: ")
+#     list_of_strings.append(row)
+#     count += 1
+#
+# print(''.join(list_of_strings))
