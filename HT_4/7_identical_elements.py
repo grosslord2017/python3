a17de9f1460dc99e2835d74277c98943ccf02e10
grosslord2017 <grosslord2017@ukr.net>
2021-11-26 11:02:59 +0200

    change 7 task

--- HT_4/7_identical_elements.py
+++ HT_4/7_identical_elements.py
@@ -8,7 +8,17 @@ def number_of_idential(arr):
             result[str(i)] = 1
         else:
             result[str(i)] += 1
-    return result
+
+    out_result = ''
+    for k, v in result.items():
+        buffer = f'{k} - {v} '
+        if v == 1:
+            buffer += 'time '
+        else:
+            buffer += 'times '
+        out_result += buffer
+
+    return out_result
 
 test = number_of_idential([2, 4, 6, 8, 3, 8, 5, 4, 2, 2, 2, 8])
 print(test)
